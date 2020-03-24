b7741ae31ecb9dc5484e21c10836aec45a86dcdb
Linda Ojinnaka <lindaojinnaka@Lindas-MacBook-Air.local>
2020-03-24 23:12:12 +1100

    changed variable names to match hashes and added two if statements.

--- pocket_app.rb
+++ pocket_app.rb
@@ -3,7 +3,7 @@ require "tty-prompt"
 
 
 def ask_question (question, options = {})# defining a method called ask_question that takes two parameters.
-    prompt = TTY::Prompt.new #creating a new prompt with the gem 
+    prompt = TTY::Prompt.new #creating a new prompt with the gem. nested it inside the ask_question method 
     if options.length == 0 # its asking if we have no options 
         return prompt.ask(question) # if so, ask question and return what they type in. they press enter and its returned back to the caller 
     else 
@@ -28,9 +28,9 @@ puts "Hello #{name}"
 
 time = ask_question(
     "How much time do we have today?", {
-        "15 minutes" => 15,
+        "15 minutes" => 15, #saved hashes so i wouldnt have to type the whole prompt in my if statements 
         "30 min" => 30, 
-        "1 hour" => 60
+        "1 hour" => 60 
         }) #first paarameter is string, the second is an array with 3 values. The array's values has to be in quation marks to make it a string 
 
 
@@ -42,7 +42,7 @@ location = ask_question(
 
 mental_state = ask_question(
     "How are you feeling today?", {
-        "I am feeling tired" => "Tired",
+        "I am feeling tired" => "Tired", #saved hashes so i wouldnt have to type the whole prompt in my if statements 
         "I am feeling anxious" => "Anxious",
         "I feel great" => "Great",
         "I have a mental block" => "Block"
@@ -55,3 +55,46 @@ end
 if location == "Desk" && mental_state == "Block" && time >=30
     cues([ "Seated Cat/Cow", "Seated Mountain", "Seated Twist", "Eagle Arms", "Seated Twist", "Seated Savasana"])
 end 
+
+
+if time == 15
+    cues(["Seated Cat/Cow, Seated Mountain, Seated Twist, Eagle Arms, Seated Savasana"])
+end
+
+# For Z
+if location == "Desk" && (mental_state == "Anxious" || mental_state == "Tired") && time == 30
+    cues(["Puppy dog with chair" ,"Seated Side Stretch" ,"Seated Twist","Seated mountain", "Seated Savasana"])
+end 
+
+# For Y
+
+if location == "room" and mental_state == "Blocked" && (time > 15)
+    cues(["Childs Pose","Cat/Cow", "Mountain", "Standing Twist", "Eagle Arms", "Bridge","Reclined Savasana"])
+end 
+
+# For C  
+if location == "Desk" && (mental_state == "Anxious" || mental_state == "Tired") && time == 60  
+    cues(["Seated Cat/Cow","Seated Mountain","Seated Twist","Sun Salutation with chair","Eagle", "Tree", "Seated Twist","Seated Savasana" ])   
+end 
+
+# For D 
+if location == "Desk" && mental_state == "Great" && time >= 15
+    cues(["Seated Cat/Cow", "Seated Mountain", "Seated Twist", "Eagle Arms", "Seated Twist", "Seated Savasana"])
+end 
+
+if location == "room" && mental_state == "Great" && time > 15 
+    cues(["Cat Cow", "Thread the Needle", "Down Dog" "Sun A", "Sun B" "Warrior 1", "Warrior 2", "Warrior 3", "Childs", "Savasana"])
+end 
+#For Y ])
+
+if location == "room" && mental_state != "Mental Block"
+    if time == 30
+    cues (["Corpse Pose", "Knee to Chest", "Reclined Twist", "Down Dog", "Sun A", "Sun B", "Wide Legged Forward Fold", "Mountain", "Happy Baby", "Savasana"]) 
+    elsif time == 60
+    puts "E"
+    cues (["Hero Pose", "Thread the Needle","Down Dog","Down Dog Plank Cycle", "Down Dog", "Mountain","Eagle", "Sun Salutation A", "Sun Salutation B","Eagle (x2)","Tree (x2)","Legs up the Wall", "Savasana"])
+end
+end  
+
+
+#For E 
